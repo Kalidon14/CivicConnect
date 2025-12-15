962a8ae8a7274fcf49fbd910e3d797fc839b971d
Kalidon14 <hammedmanial@gmail.com>
2025-12-15 15:29:40 +0800

    Try ulit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+import logo from './assets/logo.svg'
+
+ref
 
 </script>
 
 <template>
   <h1>Done Setup Project</h1>
   <p>Install other dependencies Amag inshaallah</p>
-  <p>Hello World</p>
+  <div class="logo-image">
+    <img :src="logo" />
+  </div>
 </template>
 
 <style scoped>
